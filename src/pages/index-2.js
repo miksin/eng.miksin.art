ba6acaf0441d92e467e112026d897f90eeb8a073
Miksin <ppy951@gmail.com>
2020-01-30 12:35:48 +0900

    visibility example

--- src/pages/index-2.js
+++ src/pages/index-2.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import styled from "styled-components"
+import VisibilitySensor from "react-visibility-sensor"
 
 import Banner from "../components/Banner"
 import TypingDisplay from "../components/TypingDisplay"
@@ -9,9 +10,22 @@ import * as homeConst from "../constants/home"
 
 const Block = styled.div`
   min-height: 600px;
+  border-style: solid;
+  border-width: 2px;
+`
+
+const DetectionArea = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 1px;
+  left: 0;
+  top: 0;
+  opacity: 0;
 `
 
 const IndexPage = () => {
+  const detectRef = useRef(null)
+
   const [vh, setVh] = useState(undefined)
   useEffect(() => {
     /* eslint-disable no-undef */
@@ -27,6 +41,7 @@ const IndexPage = () => {
 
   return (
     <>
+      <DetectionArea ref={detectRef} />
       <Banner styles={bannerStyles}>
         <TypingDisplay
           size={homeConst.TITLE_SIZE}
@@ -42,10 +57,34 @@ const IndexPage = () => {
           onClick={() => scrollToAnchor('test-1')}
         />
       </Banner>
-      <Block id="test-1" />
-      <Block id="test-2" />
-      <Block id="test-3" />
-      <Block id="test-4" />
+      <VisibilitySensor
+        containment={detectRef.current}
+        partialVisibility={['top', 'bottom', 'left', 'right']}
+        onChange={(e) => console.log('test-1', e)}
+      >
+        <Block id="test-1" />
+      </VisibilitySensor>
+      <VisibilitySensor
+        containment={detectRef.current}
+        partialVisibility={['top', 'bottom', 'left', 'right']}
+        onChange={(e) => console.log('test-2', e)}
+      >
+        <Block id="test-2" />
+      </VisibilitySensor>
+      <VisibilitySensor
+        containment={detectRef.current}
+        partialVisibility={['top', 'bottom', 'left', 'right']}
+        onChange={(e) => console.log('test-3', e)}
+      >
+        <Block id="test-3" />
+      </VisibilitySensor>
+      <VisibilitySensor
+        containment={detectRef.current}
+        partialVisibility={['top', 'bottom', 'left', 'right']}
+        onChange={(e) => console.log('test-4', e)}
+      >
+        <Block id="test-4" />
+      </VisibilitySensor>
     </>
   )
 }
