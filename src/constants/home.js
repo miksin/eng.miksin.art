b70c13c54581dad4e14230dd9ef80ce52ef38874
Miksin <ppy951@gmail.com>
2020-02-04 10:56:30 +0900

    line-height & about message (WIP)

--- src/constants/home.js
+++ src/constants/home.js
@@ -20,8 +20,16 @@ export const sizes = {
 
 export const about = {
   'tw': `
-台灣人，現居於日本東京擔任軟體工程師。
-軟體技能以web為中心，主要專攻前端網頁，但對於後端系統、資料庫、雲端領域等皆有涉獵。
-最擅長JavaScript/TypeScript, HTML, CSS/SCSS等。其他興趣為繪圖、遊戲、卡拉OK等。
-`
+  台灣人，現居於日本東京擔任軟體工程師。
+  軟體技能以web為中心，主要專攻前端網頁，但對於後端系統、資料庫、雲端領域等皆有涉獵。最擅長JavaScript(React/Vue), HTML, CSS/SCSS等。
+  其他興趣為繪圖、遊戲、卡拉OK等。偶爾也會在Twitter, Instagram等SNS上分享自己的創作。
+  `,
+  'en': `
+  Taiwanese and being a software engineer in Tokyo.
+  `,
+  'jp': `
+  台湾出身、東京にソフトウェアエンジニアとして働いている。
+  web中心に様々なスキルを身につけた。フロントエンド専攻ですが、バックエンド、データベース、及びクラウドなどことも渉猟した。得意分野はJavascript(React/Vue)、HTML、CSS/SCSSなど。
+  趣味は絵描く、ゲーム、カラオケなど。たまにTwitter、InstagramなどSNSで自分の作品をシェアする。
+  `,
 }
