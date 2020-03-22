e7b4b766b02a41fdd6a56f6f00e1996989e8ed4e
miksin <ppy951@gmail.com>
2020-03-22 19:43:51 +0900

    blog page base layout

---
+++ src/constants/blog.js
@@ -0,0 +1,9 @@
+import * as common from "@constants/common"
+
+export const devices = common.devices
+export const colors = common.colors
+export const icons = common.featherIcons
+
+export const sizes = {
+  ...common.sizes,
+}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,110 +1,82 @@
 import React, { useState } from "react"
-import PropTypes from "prop-types"
-import { graphql, Link } from "gatsby"
+import { graphql, useStaticQuery, Link } from "gatsby"
+import styled from "styled-components"
+import _ from "lodash"
 import { useBottomScrollListener } from "react-bottom-scroll-listener"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const Blog = ({
-  data,
-  displayPerTime,
-  location,
-}) => {
-  // extend display length when detect scroll to bottom
-  const newDisplayNum = now => Math.min(data.posts.edges.length, now + displayPerTime)
-  const [display, setDisplay] = useState(newDisplayNum(0))
-  useBottomScrollListener(() => {
-    setDisplay(newDisplayNum(display))
-  })
+import FlexBox from "@components/basic/FlexBox"
+import Seo from "@components/seo"
+import Nav from "@components/common/Nav"
+import TopPad from "@components/common/TopPad"
+import ListPreview from "@components/home/ListPreview"
+import { colors, sizes, devices } from "@constants/blog"
+import { hexToRgba } from "@src/helpers"
 
-  const posts = data.posts.edges.slice(0, display).map(edge => {
-    const { frontmatter, html } = edge.node
+const Base = styled(FlexBox)`
+  min-height: 100vh;
+`
 
-    return {
-      id: frontmatter.path,
-      html,
-      frontmatter: edge.node.frontmatter,
-    }
-  })
+const Wrapper = styled(FlexBox)`
+  width: 100%;
+  max-width: ${devices.tablet}px;
+  margin: 0 auto;
+  padding: 12px;
+`
 
-  return (
-    <Layout location={location}>
-      <SEO title="Blog" />
-      <div className="blog-preview">
-        {
-          posts.map(post => (
-            <div className="blog-preview-item" key={post.id}>
-              <h2>{post.frontmatter.title}</h2>
-              <h4>{post.frontmatter.date}</h4>
-              <div className="split-line" />
-              <div
-                className="blog-preview-content"
-                dangerouslySetInnerHTML={{ __html: post.html}}
-              />
-              <Link to={post.frontmatter.path}>
-                Read More
-              </Link>
-            </div>
-          ))
-        }
-        <div className="display-num-nav">
-          <h4>{display} / {data.posts.edges.length} Posts</h4>
-        </div>
-      </div>
-    </Layout>
-  )
-}
+const Blog = () => {
+  const data = useStaticQuery(graphql`
+    query BlogPageQuery {
+      site {
+        ...SiteMetaFragment
+      }
 
-export const query = graphql`
-  query BlogPageQuery {
-    posts: allMarkdownRemark(
-      filter: {frontmatter: {path: {regex: "/\/blog\/.*/"}}}
-      sort: { order: DESC, fields: [frontmatter___date] }
-    ) {
-      edges {
-        node {
-          html
-          frontmatter {
-            path
-            title
-            excerpt
-            fromNow: date(fromNow: true)
-            date: date(formatString: "YYYY-MM-DD")
+      posts: allMarkdownRemark (
+        filter: {frontmatter: {path: {regex: "/\/blog\/.*/"}}}
+        sort: { order: DESC, fields: [frontmatter___date] }
+      ) {
+        ...ArticlePreviewFragment
+      }
+
+      nonSenseImages: allFile (
+        filter: {extension: {regex: "/(jpg)|(png)|(jpeg)/"}, relativeDirectory: {eq: "nonsense"}}
+      ) {
+        edges {
+          node {
+            ...FeaturedImageFragment
           }
         }
       }
-      tags: group(field: frontmatter___tags){
-        fieldValue
-        totalCount
-      }
     }
-  }
-`
+  `)
 
-Blog.propTypes = {
-  displayPerTime: PropTypes.number,
-  data: PropTypes.shape({
-    posts: PropTypes.shape({
-      edges: PropTypes.arrayOf(PropTypes.shape({
-        node: PropTypes.shape({
-          html: PropTypes.string,
-          frontmatter: PropTypes.shape({
-            path: PropTypes.string.isRequired,
-            title: PropTypes.string,
-            excerpt: PropTypes.string,
-            fromNow: PropTypes.string,
-            date: PropTypes.string,
-          }).isRequired
-        }).isRequired
-      })).isRequired
-    }).isRequired
-  }).isRequired,
-  location: PropTypes.object.isRequired,
-}
+  const {
+    links,
+  } = data.site.siteMetadata
 
-Blog.defaultProps = {
-  displayPerTime: 10,
+  const [displayNum, setDisplayNum] = useState(15)
+  const posts = data.posts.edges.map(edge => edge.node.frontmatter).slice(0, displayNum)
+  const nonSenseImages = _.shuffle(data.nonSenseImages.edges.map(edge => edge.node.childImageSharp.fluid.src))
+
+  return (
+    <>
+      <Seo title="Blog" />
+      <Nav
+        links={links}
+        bgColor={hexToRgba(colors.white, 0.7)}
+        iconColor={colors.lightBlue}
+      />
+      <Base column center>
+        <TopPad />
+        <Wrapper>
+          <ListPreview articles={posts.map((frontmatter, index) => ({
+            ...frontmatter,
+            thumbnailSrc: nonSenseImages[index % nonSenseImages.length],
+            thumbnailAlt: 'post',
+          }))} />
+        </Wrapper>
+      </Base>
+    </>
+  )
 }
 
 export default Blog
-
